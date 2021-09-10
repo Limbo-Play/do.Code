e5cd03a4af37924417b2c91b987db266bf0d4565
Limbo-Play <billiabend@gmail.com>
2021-09-10 12:16:23 +0300

    makeFunct

--- arrHomeWork3.js
+++ arrHomeWork3.js
@@ -1,7 +1,7 @@
 
 const words = ['Car', 'Mango', 'Table', 'Mouse']
 const words2 = ['Hand','Mango','Phone','Mouse']
-
+/*
 //combine array to new array
 const combineArr = [...words, ...words2]
 
@@ -21,3 +21,26 @@ const arrToString = reversArr.join('-')
 
 //check result
 console.log(arrToString)
+*/
+
+
+
+//make function
+function madFunction( array, array2 ){
+    const combineArr = [...array, ...array2]
+
+    const filterUniqWord = (array) => array.filter((x, i, a) => a.indexOf(x) == i)
+
+    const uniqArr = filterUniqWord(combineArr)
+
+    const reversArr = uniqArr.map(s => [...s].reverse().join(''))
+
+    for(let i = 0; i < reversArr.length; i++){
+        reversArr[i] = reversArr[i].toUpperCase();
+    }
+
+    const arrToString = reversArr.join('-') 
+    console.log(arrToString)
+}
+
+madFunction(words, words2)
