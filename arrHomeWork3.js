0d8951766fc0dba3eabcb6c1bfc329d7323e647a
Limbo-Play <billiabend.gmail.com>
2021-09-13 18:37:42 +0300

    reName

--- arrHomeWork3.js
+++ arrHomeWork3.js
@@ -1,27 +1,7 @@
 
 const words = ['Car', 'Mango', 'Table', 'Mouse']
 const words2 = ['Hand','Mango','Phone','Mouse']
-/*
-//combine array to new array
-const combineArr = [...words, ...words2]
 
-//filter array to new array
-const filterUniqWord = (array) => array.filter((x, i, a) => a.indexOf(x) == i)
-const uniqArr = filterUniqWord(combineArr)
-
-
-//revers word and change every element toUpperCase  
-const reversArr = uniqArr.map(s => [...s].reverse().join(''))
-for(let i = 0; i < reversArr.length; i++){
-    reversArr[i] = reversArr[i].toUpperCase();
-}
-
-//changes array to string
-const arrToString = reversArr.join('-')
-
-//check result
-console.log(arrToString)
-*/
 //make function
 function madFunction( array, array2 ){
     const combineArr = [...array, ...array2]
@@ -42,3 +22,24 @@ function madFunction( array, array2 ){
 
 madFunction(words, words2)
 
+
+/*
+//combine array to new array
+const combineArr = [...words, ...words2]
+
+//filter array to new array
+const filterUniqWord = (array) => array.filter((x, i, a) => a.indexOf(x) == i)
+const uniqArr = filterUniqWord(combineArr)
+
+//revers word and change every element toUpperCase  
+const reversArr = uniqArr.map(s => [...s].reverse().join(''))
+for(let i = 0; i < reversArr.length; i++){
+    reversArr[i] = reversArr[i].toUpperCase();
+}
+
+//changes array to string
+const arrToString = reversArr.join('-')
+
+//check result
+console.log(arrToString)
+*/
